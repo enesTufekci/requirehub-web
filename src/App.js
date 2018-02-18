9e5c3e6d80942f07085235337451dc36d4677cde
max funk <mxfactorial@users.noreply.github.com>
2018-02-17 23:59:39 -0800

    Add react client

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="App-title">Welcome to RequireHub.com</h1>
+        </header>
+        <p className="App-intro">
+          To help engineers find work while lowering the cost of development, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
