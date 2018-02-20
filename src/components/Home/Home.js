11929d101a46b6d131fb3fca3d057f81149ac860
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-02-21 01:38:05 +0300

    Update - render with requirements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,11 +5,22 @@ import './Home.css';
 
 class Home extends Component {
   render() {
+    const { requirements } = this.props;
+    const requirementList = requirements.map((requirement) => 
+      (
+        <div className="case-item">
+          <Link to={`/requirement/${requirement.id}`} className="requirement-item">
+            <span>{ requirement.title}</span>
+          </Link>
+        </div>
+      )
+    );
     return (
       <div className="home-container">
         <div className="case-list">
+          { requirementList }
           <div className="case-item case-item-new">
-            <span>My First Requirement</span>
+            <span>{ requirementList.length > 0 ? 'Create New Requirement' : 'My First Requirement' }</span>
             <Link to="/requirement/create" className="circle black">+</Link>
           </div>
         </div>
