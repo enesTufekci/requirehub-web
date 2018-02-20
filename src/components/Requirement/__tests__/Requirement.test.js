8f195c7bbe1659dae1619744848cb60eca556423
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-02-20 20:51:13 +0300

    Requirement Component initial tests

--- src/components/Requirement/__tests__/Requirement.test.js
+++ src/components/Requirement/__tests__/Requirement.test.js
@@ -5,8 +5,23 @@ import Requirement from '../Requirement';
 import RequirementForm from '../RequirementForm';
 
 describe('Requirement Component', () => {
+  const mockPush = jest.fn();
+    const props = {
+      createRequirement: jest.fn(),
+      history: {
+        push: mockPush,
+      }
+    };
+  const wrapper = mount(<Requirement { ...props } />);
+
   it('should render form', () => {
-    const wrapper = mount(<Requirement />);
     expect(wrapper.find(RequirementForm).length).toEqual(1);
   });
+  
+  it('should push history after form submit', () => {
+    const form = wrapper.find(RequirementForm);
+    form.props().onSubmit('mock');
+    expect(mockPush.mock.calls.length).toBe(1);
+  });
+
 });
