bd5afcdeafb86dc546e7f2363b71076e0c72b983
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-02-20 03:00:12 +0300

    Update - Requirement route path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
 import Home from './components/Home';
-import Case from './components/Case';
+import Requirement from './components/Requirement';
 
 import './App.css';
 
@@ -16,7 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/case/:id" component={Case} />
+          <Route exact path="/requirement/create" component={Requirement} />
         </Switch>
       </BrowserRouter>
     </div>
