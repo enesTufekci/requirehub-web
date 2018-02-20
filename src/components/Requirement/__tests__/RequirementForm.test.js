acd07babbb0a7c6c4e68dd414d53a3634c29a300
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-02-20 12:48:16 +0300

    requirement form functional tests

--- src/components/Requirement/__tests__/RequirementForm.test.js
+++ src/components/Requirement/__tests__/RequirementForm.test.js
@@ -12,4 +12,40 @@ describe('RequirementForm', () => {
     expect(wrapper.find('input[name="expected"]').length).toEqual(1);
     expect(wrapper.find('button.form-submit').length).toEqual(1);
   });
+  it('it should update state on input change', () => {
+    const onSubmitMock = jest.fn();
+    const wrapper = shallow(<RequirementForm onSubmit={onSubmitMock}/>);
+    const expectedTitle = 'title';
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        value: expectedTitle,
+        getAttribute: (attr) => 'title',
+       },
+    });
+    expect(wrapper.state().values.title).toEqual(expectedTitle);
+
+    const expectedSteps = 'steps';
+    wrapper.find('textarea[name="steps"]').simulate('change', {
+      target: {
+        value: expectedSteps,
+        getAttribute: (attr) => 'steps',
+       },
+    });
+    expect(wrapper.state().values.steps).toEqual(expectedSteps);
+    
+    const expectedExpected = 'expected';
+    wrapper.find('input[name="expected"]').simulate('change', {
+      target: {
+        value: expectedExpected,
+        getAttribute: (attr) => 'expected',
+       },
+    });
+    expect(wrapper.state().values.expected).toEqual(expectedExpected);
+    const preventDefaultMock = jest.fn();
+    wrapper.find('button.form-submit').simulate('click', {
+      preventDefault: preventDefaultMock
+    });
+    expect(preventDefaultMock.mock.calls.length).toBe(1);
+    expect(onSubmitMock.mock.calls.length).toBe(1);
+  });
 });
