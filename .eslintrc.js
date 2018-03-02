7684a9ed72fe9d1b8c66b01287bb33950288fcb2
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-02 12:46:54 +0300

    eslint parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
-    "extends": [
-        "standard",
-        "standard-react",
-        "prettier-standard",
-    ],
-    "plugins": [
-        "jest"
-    ],
-    "rules": {
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error"
-    },
-    "env": {
-        "jest/globals": true
-    }
+	"extends": [
+		"standard",
+		"standard-react",
+		"prettier-standard",
+	],
+	"plugins": [
+		"jest"
+	],
+	"rules": {
+		"jest/no-disabled-tests": "warn",
+		"jest/no-focused-tests": "error",
+		"jest/no-identical-title": "error",
+		"jest/prefer-to-have-length": "warn",
+		"jest/valid-expect": "error"
+	},
+	"env": {
+		"jest/globals": true
+	},
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		}
+	},
 };
