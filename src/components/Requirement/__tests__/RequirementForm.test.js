ab821b9af96ec062c71422ed5cd5df59cbbe3661
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-25 00:33:43 +0300

    initial value tests

--- src/components/Requirement/__tests__/RequirementForm.test.js
+++ src/components/Requirement/__tests__/RequirementForm.test.js
@@ -12,6 +12,23 @@ describe('RequirementForm', () => {
     expect(wrapper.find('input[name="expected"]')).toHaveLength(1)
     expect(wrapper.find('button.form-submit')).toHaveLength(1)
   })
+  it('should be rendered correctly with initial values', () => {
+    const values = {
+      title: 'title',
+      steps: 'Steps',
+      expected: 'expected'
+    }
+    const onSubmitMock = jest.fn()
+    const wrapper = shallow(
+      <RequirementForm values={values} onSubmit={onSubmitMock} />
+    )
+    const titleInput = wrapper.find('input[name="title"]').first()
+    const stepsInput = wrapper.find('textarea[name="steps"]').first()
+    const expectedInput = wrapper.find('input[name="expected"]').first()
+    expect(titleInput.prop('value')).toBe(values.title)
+    expect(stepsInput.prop('value')).toBe(values.steps)
+    expect(expectedInput.prop('value')).toBe(values.expected)
+  })
   it('it should update state on input change', () => {
     const onSubmitMock = jest.fn()
     const wrapper = shallow(<RequirementForm onSubmit={onSubmitMock} />)
