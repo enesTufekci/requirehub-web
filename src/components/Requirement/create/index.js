819b61807cbdc0d876207030bcc823335aaf5a29
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-25 14:05:56 +0300

    createRequirement is living in requirementReducer now

--- src/components/Requirement/create/index.js
+++ src/components/Requirement/create/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 
-import { createRequirement } from '../../../appReducer'
+import { createRequirement } from '../requirementReducer'
 import RequirementCreate from './RequirementCreate'
 
 export default connect(null, { createRequirement })(RequirementCreate)
