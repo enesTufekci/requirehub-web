f697ed5ad03c49cc32972529372a98cd6b0d808d
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-25 13:40:53 +0300

    old Requirement test moved its own folder

---
+++ src/components/Requirement/create/__tests__/RequirementCreate.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import RequirementCreate from '../RequirementCreate'
+import RequirementForm from '../../RequirementForm'
+
+describe('Requirement Component', () => {
+  const mockPush = jest.fn()
+  const props = {
+    createRequirement: jest.fn(),
+    history: {
+      push: mockPush
+    }
+  }
+  const wrapper = mount(<RequirementCreate {...props} />)
+
+  it('should render form', () => {
+    expect(wrapper.find(RequirementForm)).toHaveLength(1)
+  })
+
+  it('should push history after form submit', () => {
+    const form = wrapper.find(RequirementForm)
+    form.props().onSubmit('mock')
+    expect(mockPush.mock.calls).toHaveLength(1)
+  })
+})
