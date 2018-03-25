2e47ad636fe60310f0a50d4fb7c131cba8202399
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-25 14:09:21 +0300

    requirement edit route

--- src/components/Requirement/index.js
+++ src/components/Requirement/index.js
@@ -3,12 +3,14 @@ import ReactRouterPropTypes from 'react-router-prop-types'
 
 import { Switch, Route } from 'react-router-dom'
 import RequirementCreate from './create'
+import RequirementEdit from './edit'
 import './Requirement.css'
 
 const Requirement = ({ match }) => {
   return (
     <Switch>
       <Route path={`${match.url}/create`} component={RequirementCreate} />
+      <Route path={`${match.url}/edit/:id`} component={RequirementEdit} />
     </Switch>
   )
 }
