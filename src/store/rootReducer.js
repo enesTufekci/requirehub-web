75000d19d297d9bd13a74abde1efa9d749be4c23
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-25 14:06:24 +0300

    requirement reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import appReducer from '../appReducer'
+import requirementReducer from 'components/Requirement/requirementReducer'
 
 export default combineReducers({
-  app: appReducer
+  app: appReducer,
+  requirement: requirementReducer
 })
