784261ef6256eee53a9422d65d298c98738abf85
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-27 21:39:11 +0300

    fixing media imports

--- src/components/Home/CaseHeader/index.js
+++ src/components/Home/CaseHeader/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import requireHubLogo from 'assets/images/requirehub-logo.png'
-import gitHubLogo from 'assets/images/github-logo.png'
-
 import './CaseHeader.css'
+
+const requireHubLogo = require('assets/images/requirehub-logo.png')
+const gitHubLogo = require('assets/images/github-logo.png')
 
 const CaseHeader = () => {
   return (
     <div className="case-header">
-        <img className="case-header-requirehub" src={requireHubLogo} alt="RequireHub Logo" />
-        <span className="case-header-on"> on </span>
-        <img className="case-header-github" src={gitHubLogo} alt="Github Logo" />
+      <img
+        className="case-header-requirehub"
+        src={requireHubLogo}
+        alt="RequireHub Logo"
+      />
+      <span className="case-header-on"> on </span>
+      <img className="case-header-github" src={gitHubLogo} alt="Github Logo" />
     </div>
   )
 }
