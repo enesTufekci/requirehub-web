50644485246f2313deacc74124e166160d4c5667
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-28 20:36:50 +0300

    update reducer tests

--- src/components/Requirement/__tests__/requirementReducer.test.js
+++ src/components/Requirement/__tests__/requirementReducer.test.js
@@ -1,6 +1,9 @@
 // import configureMockStore from 'redux-mock-store'
-import { CREATE_REQUIREMENT } from 'common/actionTypes'
-import requirementReducer, { createRequirement } from '../requirementReducer'
+import { CREATE_REQUIREMENT, UPDATE_REQUIREMENT } from 'common/actionTypes'
+import requirementReducer, {
+  createRequirement,
+  updateRequirement
+} from '../requirementReducer'
 
 // const middlewares = []
 // const mockStore = configureMockStore(middlewares)
@@ -19,6 +22,20 @@ describe('requirementReducer', () => {
       requirements: [...initialState.requirements, mockRequirement]
     })
   })
+
+  test('handles UPDATE_REQUIREMENT action', () => {
+    const mockRequirements = [{ id: 1, title: 'test requirement' }]
+    const initialState = { requirements: mockRequirements }
+    const mockRequirementUpdated = { id: 1, title: 'test requirement update' }
+    const mockAction = {
+      type: UPDATE_REQUIREMENT,
+      payload: mockRequirementUpdated
+    }
+    expect(requirementReducer(initialState, mockAction)).toEqual({
+      ...initialState,
+      requirements: [mockRequirementUpdated]
+    })
+  })
 })
 
 describe('#createRequirement', () => {
@@ -29,5 +46,16 @@ describe('#createRequirement', () => {
       payload: mockRequirement
     }
     expect(createRequirement(mockRequirement)).toEqual(expectedAction)
+  })
+})
+
+describe('#updateRequirement', () => {
+  it('should create UPDATE_REQUIREMENT action to update requirements', () => {
+    const mockRequirement = { id: 1, title: 'test requirement' }
+    const expectedAction = {
+      type: UPDATE_REQUIREMENT,
+      payload: mockRequirement
+    }
+    expect(updateRequirement(mockRequirement)).toEqual(expectedAction)
   })
 })
