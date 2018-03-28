5df1f98865d7d4424f6c5d23edc4ce4c0517af58
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-28 20:49:40 +0300

    converted to class component

--- src/components/Requirement/edit/RequirementEdit.js
+++ src/components/Requirement/edit/RequirementEdit.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactRouterPropTypes from 'react-router-prop-types'
 import RequirementForm from '../common/RequirementForm'
 
-const RequirementEdit = ({ requirement }) => {
-  return (
-    <div className="requirement-container">
-      <RequirementForm values={requirement} onSubmit={() => {}} />
-    </div>
-  )
-}
-
-RequirementEdit.propTypes = {
-  requirement: PropTypes.shape({
-    uuid: PropTypes.string,
-    title: PropTypes.string,
-    steps: PropTypes.string,
-    expected: PropTypes.string
-  })
+class RequirementEdit extends React.Component {
+  static propTypes = {
+    requirement: PropTypes.shape({
+      uuid: PropTypes.string,
+      title: PropTypes.string,
+      steps: PropTypes.string,
+      expected: PropTypes.string
+    }).isRequired,
+    updateRequirement: PropTypes.func.isRequired,
+    history: ReactRouterPropTypes.history
+  }
+  handleUpdate = updatedValues => {
+    const { updateRequirement, history } = this.props
+    updateRequirement(updatedValues)
+    history.push('/')
+  }
+  render() {
+    const { requirement } = this.props
+    return (
+      <div className="requirement-container">
+        <RequirementForm values={requirement} onSubmit={this.handleUpdate} />
+      </div>
+    )
+  }
 }
 
 export default RequirementEdit
