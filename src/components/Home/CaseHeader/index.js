8a8e71f5d754bf7e0c02cf6dfe6ad1e588ee8334
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-28 19:16:08 +0300

    Revert "fixing media imports"
    This reverts commit 784261ef6256eee53a9422d65d298c98738abf85.

--- src/components/Home/CaseHeader/index.js
+++ src/components/Home/CaseHeader/index.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import './CaseHeader.css'
+import requireHubLogo from 'assets/images/requirehub-logo.png'
+import gitHubLogo from 'assets/images/github-logo.png'
 
-const requireHubLogo = require('assets/images/requirehub-logo.png')
-const gitHubLogo = require('assets/images/github-logo.png')
+import './CaseHeader.css'
 
 const CaseHeader = () => {
   return (
     <div className="case-header">
-      <img
-        className="case-header-requirehub"
-        src={requireHubLogo}
-        alt="RequireHub Logo"
-      />
-      <span className="case-header-on"> on </span>
-      <img className="case-header-github" src={gitHubLogo} alt="Github Logo" />
+        <img className="case-header-requirehub" src={requireHubLogo} alt="RequireHub Logo" />
+        <span className="case-header-on"> on </span>
+        <img className="case-header-github" src={gitHubLogo} alt="Github Logo" />
     </div>
   )
 }
