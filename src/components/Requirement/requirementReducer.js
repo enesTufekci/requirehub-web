1cf8de96290a8f0a4246a102945b7431d2038db4
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-28 20:35:29 +0300

    update reducer

--- src/components/Requirement/requirementReducer.js
+++ src/components/Requirement/requirementReducer.js
@@ -1,7 +1,12 @@
-import { CREATE_REQUIREMENT } from 'common/actionTypes'
+import { CREATE_REQUIREMENT, UPDATE_REQUIREMENT } from 'common/actionTypes'
 
 export const createRequirement = requirement => ({
   type: CREATE_REQUIREMENT,
+  payload: requirement
+})
+
+export const updateRequirement = requirement => ({
+  type: UPDATE_REQUIREMENT,
   payload: requirement
 })
 
@@ -9,6 +14,17 @@ const ACTION_HANDLERS = {
   [CREATE_REQUIREMENT]: (state, action) => ({
     ...state,
     requirements: [...state.requirements, action.payload]
+  }),
+  [UPDATE_REQUIREMENT]: (state, action) => ({
+    ...state,
+    requirements: [
+      ...state.requirements.map(requirement => {
+        if (requirement.uuid === action.payload.uuid) {
+          return action.payload
+        }
+        return requirement
+      })
+    ]
   })
 }
 
