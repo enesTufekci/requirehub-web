7cb8b378e3614a97512b046482d7fab937651b5e
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-03-28 20:54:54 +0300

    mock props

--- src/components/Requirement/edit/__tests__/RequirementEdit.test.js
+++ src/components/Requirement/edit/__tests__/RequirementEdit.test.js
@@ -4,14 +4,19 @@ import RequirementForm from '../../common/RequirementForm'
 import { shallow } from 'enzyme'
 
 describe('<RequirementEdit />', () => {
+  const updateRequirementMock = jest.fn()
   const requirementMock = {
     uuid: '1234-abcd',
     title: 'Mock Requirement',
     steps: 'Mock Steps',
     expected: 'Mock Expected'
   }
+  const props = {
+    requirement: requirementMock,
+    updateRequirement: updateRequirementMock
+  }
   it('should render edit form (with default values)', () => {
-    const wrapper = shallow(<RequirementEdit requirement={requirementMock} />)
+    const wrapper = shallow(<RequirementEdit {...props} />)
     expect(wrapper.find(RequirementForm)).toHaveLength(1)
     expect(wrapper.find(RequirementForm).props().values).toEqual(
       requirementMock
